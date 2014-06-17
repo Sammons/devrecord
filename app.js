285c51f457f5db5cc646dc2c48ccdbdc9b48bbb2
Sammons <bsammnz@gmail.com>
2014-06-16 23:31:52 -0500

    ua

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ app.use(favicon(__dirname+'/public/images/logo.ico'));
 app.use(express.static('./public'));
 
 app.get('/', function(inc,out) {
-    console.log(inc.is_mobile+' <--');
-    console.log(inc);
-    if (inc.is_mobile && ! /mobile/.test(inc.url)) {
-      console.log('mobile sent');
-      return inc.redirect('/?mobile=true');
-    }
-    else return out.end(renderer());
+    // console.log(inc.is_mobile+' <--');
+    // console.log(inc);
+    // if (inc.is_mobile && ! /mobile/.test(inc.url)) {
+    //   console.log('mobile sent');
+    //   return inc.redirect('/?mobile=true');
+    // }
+    // else return out.end(renderer());
 
 });
 
