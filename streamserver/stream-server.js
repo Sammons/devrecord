6a562b7bde0fb551515c37de09249ab7b809fc14
Sammons <bsammnz@gmail.com>
2014-06-17 20:31:29 -0500

    playing with video

--- streamserver/stream-server.js
+++ streamserver/stream-server.js
@@ -7,7 +7,7 @@ if( process.argv.length < 3 ) {
 	process.exit();
 }
 
-var STREAM_SECRET = process.argv[2],
+var STREAM_SECRET = process.argv[2] || pass,
 	STREAM_PORT = process.argv[3] || 8082,
 	WEBSOCKET_PORT = process.argv[4] || 8084,
 	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
