3a83238ed7ee25624991a870e7afcc4b485c97e3
Sammons <bsammnz@gmail.com>
2014-06-20 19:36:20 -0500

    created video stream script wrapper for convenience

---
+++ stream-on.js
@@ -0,0 +1,19 @@
+var child_process = require('child_process');
+var child;
+var stuff = [];
+
+function restart() {
+ if (child) child.kill();
+ child = child_process.exec('./ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video -b:v 700k -r 30 http://162.243.151.153:8082/'+process.env.PASSWORD+'/320/240/'
+	,function(e,s,er) {
+		console.log(e,s,er)
+	})
+}
+
+
+setInterval(function(){
+	try{
+	restart();
+	} catch(e){ console.log(e) }
+},1000*10)
+
