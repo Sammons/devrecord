5b622d61998443612def5bfb55cbc8545dcb2da5
Sammons <bsammnz@gmail.com>
2014-06-22 18:56:43 -0500

    debugging connections

--- spark.js
+++ spark.js
@@ -1,5 +1,10 @@
 var net = require('net');
 
+// for fun, right?
+function initpush(obj, key, elem) {
+	if (!obj[key]) obj[key] = [];
+	obj[key].push(elem);
+}
 
 var manager = function() {
 	var options = {
@@ -9,21 +14,106 @@ var manager = function() {
 		,socketCount: 0
 	}
 
-	var server = net.createServer( function( connection ) {
+	var server = this.server = net.createServer( function( connection ) {
 
-		var address = connection.address()
+		/* set up custom listener system */
+			var triggers = connection.triggers = {};
+			var oncetriggers = connection.oncetriggers = {};
+			var conditionally_once_triggers = connection.conditionally_once_triggers = {};
+			var trigger = connection.trigger = function(what, args) {
+				for (var i in triggers[what]) 
+					triggers[what][i].apply(connection,
+						Array.prototype.slice.call(arguments, 1));
+				for (var i in oncetriggers[what])
+					oncetriggers[what][i].apply(connection,
+						Array.prototype.slice.call(arguments, 1));
+				for (var i in conditionally_once_triggers[what]){
+					var bool = conditionally_once_triggers[what][i].apply(connection,
+						Array.prototype.slice.call(arguments, 1));
+					if (bool) conditionally_once_triggers[what].splice(i);
+				}
+				oncetriggers[what] = [];
+			}
+			var on = connection.on = function(what, callback) {
+				initpush(triggers, what, callback);
+			}
+			var once = connection.once = function(what, callback) {
+				initpush(oncetriggers, what, callback);
+			}
+			var conditionally_once = connection.conditionally_once = function(what, callback) {
+				initpush(conditionally_once_triggers, what, callback)
+			}
+			var unbind = connection.unbind = function(what, func) {
+				triggers[what] = removeFromArray(triggers[what], func);
+			}
+
+
+		connection.addr = connection.address();
 		
-		console.log('connection started: '+address.family+' '+address.address+' '+address.port);
+		// I heartbeat will be necessary
+		// connection.setTimeout(2000);
 
-		connection.on("data",function(data){ console.log("data recieved",data) });
+		// I'll take it fast please
+		// connection.setNoDelay(true);
 
-	} )
+		// And make it clear
+		connection.setEncoding('utf8');
 
+		console.log( 'connection added: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
+		
+		// welcome
+		connection.write('300','utf8');
+
+		connection.on("connect",function() { 
+
+			trigger("connected",connection.addr);
+
+		})
+
+		connection.on( "data",function( data ) { 
+			//trim to be sure nothing strange catches us
+			data = data.trim();
+
+			console.log( "data recieved", data );
+
+
+			var first_and_rest = data.split( /\:/ )[ 0 ];
+			
+			if ( first_and_rest != null && first_and_rest.length == 2 ) 
+			{ trigger( first_and_rest[ 0 ], first_and_rest[ 1 ] ); }
+			
+		} );
+
+		connection.on( "drain", function() { /* nothing here */
+
+			console.log( 'connection drained: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
+
+		} );
+
+		connection.on( "close", function() { /* when there is an error or it just closes */
+
+			console.log( 'connection closed: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
+
+		} );
+
+		connection.on( "timeout", function() { /* when the connection has timed out */
+
+			console.log( 'connection timed out: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
+
+		} );
+
+		connection.on( "end", function() { /* other end of connection sent a FIN */
+
+			console.log( 'connection ended: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
+
+		} );
+
+	
+	} ).listen(options.port);
 }
 
-
-
-
+var sparkLord = new manager();
+console.log(sparkLord);
 var broadcast = function(sshtuff) {
 }
 /* if this function had a name it would be
