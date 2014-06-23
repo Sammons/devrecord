a74aef37726acbba4cdc910965f2af83be9c62cd
Sammons <bsammnz@gmail.com>
2014-06-22 20:42:35 -0500

    hopefully these guys will be more stable this way

--- middleware.js
+++ middleware.js
@@ -37,7 +37,7 @@ var use_production_config = function(app) {
 var use_dev_config = function() {
 	var app = express();
 	app.set('env', /* debug = */ true)
-	app.use(morgan({format: ':method :url  || :remote-addr || :status'}))
+	// app.use(morgan({format: ':method :url  || :remote-addr || :status'}))
 	app.use(userAgent());
 	app.use(favicon(__dirname+'/public/images/logo.ico'));
 	app.use(ssl_tape());
--- spark.js
+++ spark.js
@@ -1,119 +1,77 @@
 var net = require('net');
 
+var sockets = [];
+
 // for fun, right?
 function initpush(obj, key, elem) {
 	if (!obj[key]) obj[key] = [];
 	obj[key].push(elem);
 }
 
-var manager = function() {
-	var options = {
-		 port : 6000 // default port, completely arbitrary
-		,socks: {}
-		,messageQs: {}
-		,socketCount: 0
+var connectedHandler = function() { }
+
+var ignoreHandler = function( evt ) { 
+	return function() { console.log('event ignored '+evt) } 
+};
+
+var destroyHandler = function() {
+	for (var i in sockets) {
+		if (sockets[i] == this) {
+			sockets.splice(i);
+			console.log('socket closed')
+			try{ this.destroy(); } catch(e) {}
+		}
 	}
-
-	var server = this.server = net.createServer( function( connection ) {
-
-		/* set up custom listener system */
-			var triggers = connection.triggers = {};
-			var oncetriggers = connection.oncetriggers = {};
-			var conditionally_once_triggers = connection.conditionally_once_triggers = {};
-			var trigger = connection.trigger = function(what, args) {
-				for (var i in triggers[what]) 
-					triggers[what][i].apply(connection,
-						Array.prototype.slice.call(arguments, 1));
-				for (var i in oncetriggers[what])
-					oncetriggers[what][i].apply(connection,
-						Array.prototype.slice.call(arguments, 1));
-				for (var i in conditionally_once_triggers[what]){
-					var bool = conditionally_once_triggers[what][i].apply(connection,
-						Array.prototype.slice.call(arguments, 1));
-					if (bool) conditionally_once_triggers[what].splice(i);
-				}
-				oncetriggers[what] = [];
-			}
-			var on = connection.on = function(what, callback) {
-				initpush(triggers, what, callback);
-			}
-			var once = connection.once = function(what, callback) {
-				initpush(oncetriggers, what, callback);
-			}
-			var conditionally_once = connection.conditionally_once = function(what, callback) {
-				initpush(conditionally_once_triggers, what, callback)
-			}
-			var unbind = connection.unbind = function(what, func) {
-				triggers[what] = removeFromArray(triggers[what], func);
-			}
-
-
-		connection.addr = connection.address();
-		
-		// I heartbeat will be necessary
-		// connection.setTimeout(2000);
-
-		// I'll take it fast please
-		// connection.setNoDelay(true);
-
-		// And make it clear
-		connection.setEncoding('utf8');
-
-		console.log( 'connection added: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
-		
-		// welcome
-		connection.write('300','utf8');
-
-		connection.on("connect",function() { 
-
-			trigger("connected",connection.addr);
-
-		})
-
-		connection.on( "data",function( data ) { 
-			//trim to be sure nothing strange catches us
-			data = data.trim();
-
-			console.log( "data recieved", data );
-
-
-			var first_and_rest = data.split( /\:/ )[ 0 ];
-			
-			if ( first_and_rest != null && first_and_rest.length == 2 ) 
-			{ trigger( first_and_rest[ 0 ], first_and_rest[ 1 ] ); }
-			
-		} );
-
-		connection.on( "drain", function() { /* nothing here */
-
-			console.log( 'connection drained: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
-
-		} );
-
-		connection.on( "close", function() { /* when there is an error or it just closes */
-
-			console.log( 'connection closed: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
-
-		} );
-
-		connection.on( "timeout", function() { /* when the connection has timed out */
-
-			console.log( 'connection timed out: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
-
-		} );
-
-		connection.on( "end", function() { /* other end of connection sent a FIN */
-
-			console.log( 'connection ended: ' + connection.addr.family + ' ' + connection.addr.address + ' ' + connection.addr.port );
-
-		} );
-
-	
-	} ).listen(options.port);
 }
 
-var sparkLord = new manager();
-console.log(sparkLord);
+var dataHandler = function( data ) { 
+	data = (data+'').trim();
+	/* console.log( this.address(), "sent us", data ); */
+
+	var valid_message_format = /^[A-Z]\:.*?$|^1$/gi
+	
+	if (!(valid_message_format.test(data))) ; /*console.log('invalid message format');*/
+	else {
+		var command_format = /(^.*?):(.*$)/g
+		var tokens = data.split(command_format);
+		if (tokens == null || tokens.length != 4) {
+			; /*console.log('not a message',tokens);*/
+		} else {
+			if (!(this.emit(tokens[1],tokens[2]))) ; /*console.log('message ignored');*/
+		}
+	}
+}
+
+function initSocket() {
+	this.write('welcome','utf8');
+	this.setTimeout('1000');
+	this.on( "connect", ignoreHandler( "connect") );
+	this.on( "data",    dataHandler               );
+	this.on( "timeout", destroyHandler            );
+	this.on( "close",   destroyHandler            );
+	this.on( "end",     destroyHandler            );
+	this.on( "error",   destroyHandler            );
+
+	this.dogtags = {};
+
+}
+
+var options = {
+	 port : 6000 // default port, completely arbitrary
+	,socks: {}
+	,messageQs: {}
+	,socketCount: 0
+}
+
+
+var server = net.createServer( function( socket ) {
+	console.log('socket opened:',socket.address());
+	sockets.push(socket);
+	initSocket.call(socket);
+} ).listen(options.port);
+
+
+
 var broadcast = function(sshtuff) {
 }
 /* if this function had a name it would be
@@ -126,11 +84,44 @@ module.exports = function(app) {
 
 	// rgb = red green blue = 255,235,233 for example
 	app.post('/spark/:sparkcore/:rgb',function( req, res ) {
+		console.log('recieved')
+		try {
+			var sparkid = req.params.sparkcore;
+			var colors = req.params.rgb.split(',');
+			var matches = true;
+			for (i in colors) {
+				if (!/^[0-9][0-9]?[0-9]?$/.test(colors[i]) && colors[i]/1 < 256) 
+					matches = false;
+			}
+			if (matches) {
+				if (!!sockets[sparkid]) {
+					console.log('C'+colors.join(''))
+					sockets[sparkid].write('C'+colors.join(','));
+				}
+			} else {
+				res.status(404);
+			}
+
+		} catch (e) {
+			console.log('msgerr',e);
+		}
+		res.end();
 	})
 
 	app.get('/spark/:sparkcore',function( req, res) {
+		try {
+			console.log(JSON.stringify(sockets[req.params.sparkcore].name))
+			res.end(JSON.stringify(sockets[req.params.sparkcore].name));
+		} catch (e) {
+
+		}
 	})
 
 	app.get('/spark',function(req,res) {
+		var socketDetails = [];
+		for (var i in sockets) {
+			socketDetails.push({ name: sockets[i].name })
+		}
+		res.end(JSON.stringify(socketDetails));
 	})
 }
