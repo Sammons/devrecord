b171daa7a425238eca5c154fe44cf73d6616a101
Sammons <bsammnz@gmail.com>
2014-07-07 20:07:50 -0500

    only posts here

--- github_files.js
+++
@@ -1,9 +0,0 @@
-var ejs = require('ejs');
-var fs = require('fs');
-var request = require('request');
-var md_renderer = require('./md_renderer');
-
-var old_files = fs.readdirSync('gh-pages');
-
-for (var i in old_files) fs.unlinkSync('gh-pages/'old_files[i]);
-
