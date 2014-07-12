193a635aabf5008f0cc88a929f5a3d466e8ad674
Sammons <bsammnz@gmail.com>
2014-07-12 13:25:09 -0500

    fixing tld redirect

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var vhost = require('vhost');
 var morgan = require('morgan')
-var app = require('express')();
+var express = require('express');
+var app = express();
+var tldapp = express();
 var server = require('http').Server(app);
 var post_server = require('./post_server/static-server.js');
 var spark_server = require('./sparkcore_server/spark.js');
@@ -8,10 +10,11 @@ var camera_server = require('./camera_server/stream-server.js');
 
 var TLD = 'devrecord.com'
 
-app.get('/',function(i,o){o.redirect('http://posts.devrecord.com');});
-app.get('/ben',function(i,o){o.redirect('http://ben.devrecord.com');});
+tldapp.get('/',function(i,o){o.redirect('http://posts.devrecord.com');});
+tldapp.get('/ben',function(i,o){o.redirect('http://ben.devrecord.com');});
 
-app.use(morgan('default'))
+app.use(morgan('default'));
+app.use(vhost(TLD,tldapp));
 app.use(vhost("posts.devrecord.com", post_server));
 
 server.listen(process.env.PORT || 3000);
