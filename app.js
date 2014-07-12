04dfd9047b0851f0be806ebd8fb3d1f036abbd34
Sammons <bsammnz@gmail.com>
2014-07-12 13:35:04 -0500

    just a bit of cleaning

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ var post_server = require('./post_server/static-server.js');
 var spark_server = require('./sparkcore_server/spark.js');
 var camera_server = require('./camera_server/stream-server.js');
 
-var TLD = 'devrecord.com'
+var TLD = 'devrecord.com';
+var myurl = 'ben.devrecord.com';
+var posturl = 'posts.devrecord.com';
+var me = 'ben';
 
-tldapp.get('/',function(i,o){o.redirect('http://posts.devrecord.com');});
-tldapp.get('/ben',function(i,o){o.redirect('http://ben.devrecord.com');});
+tldapp.get('/',function(i,o){o.redirect( 'http://'+posturl );});
+tldapp.get('/'+me,function(i,o){o.redirect( 'http://'+myurl );});
 
 app.use(morgan('default'));
 app.use(vhost(TLD,tldapp));
-app.use(vhost("posts.devrecord.com", post_server));
+app.use(vhost(posturl, post_server));
 
 server.listen(process.env.PORT || 3000);
