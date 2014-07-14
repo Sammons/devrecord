5fcd3878603bc935f1c52eb801c20c01b4ae7c00
Sammons <bsammnz@gmail.com>
2014-07-13 19:36:22 -0500

    adding the camera sever iframe

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ var tldapp = express();
 var server = require('http').Server(app);
 var post_server = require('./post_server/static-server.js');
 var spark_server = require('./sparkcore_server/spark.js');
-var camera_server = require('./camera_server/stream-server.js');
+var camera_server = require('./camera_server/camera-server.js');
 
-var TLD = 'devrecord.com';
+var TLD = 'build.devrecord.com';
 var myurl = 'ben.devrecord.com';
 var posturl = 'posts.devrecord.com';
+var camurl = 'stream.devrecord.com';
 var me = 'ben';
 
 tldapp.get('/',function(i,o){o.redirect( 'http://'+posturl );});
@@ -19,5 +20,6 @@ tldapp.get('/'+me,function(i,o){o.redirect( 'http://'+myurl );});
 app.use(morgan('default'));
 app.use(vhost(TLD,tldapp));
 app.use(vhost(posturl, post_server));
+app.use(vhost(camurl, camera_server));
 
 server.listen(process.env.PORT || 3000);
--- camera_server/stream-on.js
+++ camera_server/stream-on.js
@@ -4,7 +4,7 @@ var stuff = [];
 
 function restart() {
  if (child) child.kill();
- child = child_process.exec('./ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video -b:v 700k -r 30 http://162.243.151.153:8082/'+process.env.PASSWORD+'/320/240/'
+ child = child_process.exec('./ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video -b:v 700k -r 30 http://162.243.151.153:8082/'+process.env.CAM_PASS+'/320/240/'
 	,function(e,s,er) {
 		console.log(e,s,er)
 	})
