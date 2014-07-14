68cde71c99661d1feb4b6b04e48daca825811cad
Sammons <bsammnz@gmail.com>
2014-07-13 23:57:00 -0500

    camera widget basic working

--- hosting_server/public/extras.js
+++ hosting_server/public/extras.js
@@ -15,24 +15,9 @@ function begin() {
 
 	function initialize_camera_view() {
 		var $showcase = $('.showcase');
-		var client, player;
-
-		$showcase.css({
-			width: '320px',
-			height: '240px',
-			'background-color':'black'
-		});
-		var text = $('<div id="showcase-text"><a href="#showcase-text">press me to see the livestream...</a></div>').css({
-			width: '320px',
-			color: 'rgb(200,200,200)',
-			opacity: '0.5',
-			'font-size':'36px',
-			'text-align':'center',
-			'padding-top':'60px'
-		})
+		stop_cam('press me to see the livestream...');
 		var paused = true;
-		$showcase.html(text)
-			.mouseenter(
+		$showcase.mouseenter(
 			function(){
 				$('#showcase-text').animate({
 				opacity: '1'
@@ -56,23 +41,70 @@ function begin() {
 					ctx.fillRect(0,0,canvas.width, canvas.height);
 					client = new WebSocket( 'ws://stream.devrecord.com:8084' );
 					player = new jsmpeg( client, { canvas : canvas } );
+					var page = [];
+					player.nothing_changing_check = setInterval(function(){
+						if (!paused) {
+								var canvas = $('#videoCanv')[0];
+								if (!canvas) return;
+								var context = canvas.getContext('2d');
+								var data = context.getImageData(0,0,canvas.width, canvas.height);
+								for (var i = 0; i < data.data.length; i++) {
+									if (page.data && page.data[i] != data.data[i]) { 
+										page = data;
+										break; 
+									}else if (!page.data) {
+										page = data
+										break;
+									} else {
+										nothing_to_see = true;
+										stop_cam('Nothing seems to be going on on the camera right now...');
+										page = [];
+									}
+								}
+						} else {
+						}
+					},1000)
 				} else {
+					$showcase.css({
+						'background-color':'rgba(0,0,0,0)'
+					}).children().remove();
 					if (client) client.close();
 					delete player;
+					stop_cam('press me to see the livestream...');
 				}
 				paused = !paused;
 			})
+		var page = [];
+
 		setInterval(function(){
 			if (!paused) {
+				client.close();
 				client = new WebSocket( 'ws://stream.devrecord.com:8084' );
 				player = new jsmpeg( client, { canvas : canvas } );
 			}
-		}, 10*1000)
+		}, 60*1000*5)
 	}
 
-	function start_cam( canvas ) {
+	function stop_cam( t ) {
+		var $showcase = $('.showcase');
 
-
+		$showcase.css({
+			width: '320px',
+			height: '240px',
+			'background-color':'black'
+		});
+		var text = $('<div id="showcase-text">'+t+'</div>')
+			.css({
+				width: '320px',
+				 color: 'rgb(200,200,200)',
+				opacity: '0.5',
+				'font-size':'36px',
+				'text-align':'center',
+				'padding-top':'60px',
+				'text-decoration':'none',
+				'cursor':'pointer'
+			})
+		$showcase.html(text);
 
 	}
 }
