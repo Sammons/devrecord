c9d164947b6bb08cb59a935df6ae2ea55debffc1
Sammons <bsammnz@gmail.com>
2015-05-03 18:31:08 -0500

    make render engine handlebars, and pull in favicon

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+/* use handlebars */
+var handlebars = require('express-handlebars');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+app.engine('handlebars', handlebars({defaultLayout : 'main'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
