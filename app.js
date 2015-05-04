726d0799ec2f9275e73b576232bba381a3ac2c43
Sammons <bsammnz@gmail.com>
2015-05-03 19:48:25 -0500

    nuke users.js

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
