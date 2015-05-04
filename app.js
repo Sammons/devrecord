633291ca0eae3b9194bb1a2a7894df8e34efa572
Sammons <bsammnz@gmail.com>
2015-05-03 19:33:05 -0500

    clean everything out and start over

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.set('view engine', require('hbs').__express);
-
+// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
